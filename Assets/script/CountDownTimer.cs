7ba0f7cc9aa0a28c4c911dfa255f3385ec349981
Tejima Tsuyoshi <133856078+LifeLight4649@users.noreply.github.com>
2023-08-31 10:32:37 +0900

    「スタートのボタンが押される前にカウントダウンが始まってしまう」状態を編集

--- Assets/script/CountDownTimer.cs
+++ Assets/script/CountDownTimer.cs
@@ -8,6 +8,7 @@ public class CountDownTimer : GameManegar
     [SerializeField] static float CountDownTime;    // カウントダウンタイム
     public Text TextCountDown;              // 表示用テキストUI
     public static float cleartime = 99.0f;
+    bool onstart = false;
 
     // Use this for initialization
     void Start()
@@ -19,7 +20,11 @@ public class CountDownTimer : GameManegar
     void Update()
     {
         TextCountDown.text = String.Format("Time: {0:00.00}", CountDownTime);// カウントダウンタイムを整形して表示
-        CountDownTime -= Time.deltaTime;// 経過時刻を引いていく
+
+        if (onstart == true)
+        {
+            CountDownTime -= Time.deltaTime;// 経過時刻を引いていく 
+        }
 
         if (CountDownTime <= 0.0F) // 0.0秒以下になったらカウントダウンタイムを0.0で固定（止まったように見せる）
         {
@@ -40,6 +45,11 @@ public class CountDownTimer : GameManegar
         {
             Debug.Log("タイマー追加!");
         }
+    }
+
+    public void Starttime()
+    {
+      onstart = true;
     }
 
     public static void clearscene()
